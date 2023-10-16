26e712d181be1fe269c8e8e90f8959d181e67644
Kayce Basques <kaycebasques@gmail.com>
2023-10-15 19:39:18 -0700

    feat: Add FoFF (Flask on Firebase Functions) logic

--- src/server/functions/main.py
+++ src/server/functions/main.py
@@ -1,13 +1,15 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
+from firebase_functions import https_fn, options
+import flask
+import flask_cors
 
-from firebase_functions import https_fn
-from firebase_admin import initialize_app
+app = flask.Flask(__name__)
+flask_cors.CORS(app)
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+@app.get('/')
+def hello():
+    return 'Hello, world!'
+
+@https_fn.on_request(timeout_sec=60, memory=options.MemoryOption.GB_1)
+def server(req: https_fn.Request) -> https_fn.Response:
+    with app.request_context(req.environ):
+        return app.full_dispatch_request()
