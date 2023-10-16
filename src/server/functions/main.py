00991dc22289f075e428cfcff2f94e209ac6409f
Kayce Basques <kaycebasques@gmail.com>
2023-10-15 20:11:05 -0700

    fix: Change messages

--- src/server/functions/main.py
+++ src/server/functions/main.py
@@ -6,10 +6,10 @@ app = flask.Flask(__name__)
 flask_cors.CORS(app)
 
 @app.get('/')
-def hello():
-    return 'Hello, world!'
+def hello_server():
+    return 'Hello, server!'
 
 @https_fn.on_request(timeout_sec=60, memory=options.MemoryOption.GB_1)
-def server(req: https_fn.Request) -> https_fn.Response:
+def on_request(req: https_fn.Request) -> https_fn.Response:
     with app.request_context(req.environ):
         return app.full_dispatch_request()
